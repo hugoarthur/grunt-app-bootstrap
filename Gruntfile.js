d029ce6b0361bdf6b35092a13e87ca6ac19cd597
@hugoarthur <hugo220390@gmail.com>
2015-07-09 16:13:36 -0300

    Created a grunt web app with bootstap theme.

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt) {
+	// Project configuration. 
+	grunt.initConfig({
+	  concat: {
+	    js: {
+	      src: ['js/*.js'],
+	      dest: 'dist/js/built.js',
+	    },
+	    css: {
+	      src: ['css/*.css'],
+	      dest: 'dist/css/styles.css',
+	    }
+	  },
+	  jshint: {
+	    beforeconcat: ['src/**/*.js'],
+	    afterconcat: ['dist/js/built.js']
+	  },
+	  copy: {
+		  main: {
+		    files: [
+		      // includes html files and img folder in the root path
+		      {expand: true, src: ['*.html'], dest: 'dist/', filter: 'isFile'},
+		      {expand: true, src: ['img/**'], dest: 'dist/'}
+		    ],
+		  },
+		  bower: {
+		    files: [
+		      // includes files within path and its sub-directories 
+		      {expand: true, src: ['bower_components/**'], dest: 'dist/'}
+		    ],
+		  },
+		},
+		watch: {
+			configFiles: {
+				files: [ 'Gruntfile.js'],
+				options: {
+					livereload: true
+				}
+			},
+		  	scripts: {
+			    files: ['js/*.js'],
+			    tasks: ['concat:js', 'jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			styles: {
+			  	files: ['**/*.css'],
+			  	tasks: ['concat:css'],
+				options: {
+					livereload: true
+				}
+			},
+			view: {
+			  	files: ['**/*.html', '!dist/**'],
+			  	tasks: ['copy:main'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+	  connect: {
+	    server: {
+	      options: {
+	        port: 9000,
+	        base: 'dist',
+	        livereload: true,
+	        open: true
+	      }
+	    }
+	  }
+	});
+
+	grunt.registerTask('serve', function() {
+		grunt.task.run([
+			'concat',
+			'jshint',
+			'copy',
+			'connect:server',
+	    	'watch'
+	    ]);
+	});
+
+	grunt.registerTask('build', function() {
+		grunt.task.run([
+			'concat',
+			'jshint',
+			'copy'
+	    ]);
+	});
+
+	grunt.registerTask('default', function() {
+		console.log('CRUZEIRO!!!');
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+};
---
+++ README.md
@@ -0,0 +1,29 @@
+# Bootstrap theme with grunt
+
+Just a simple grunt web app with a bootstrap theme downloaded from [StartBootstrap].
+
+For this application to work it is necessary for your system to have [NodeJs], [GruntJs] and [Bower].
+
+To prepare the application run this commands:
+```
+npm install
+bower install
+```
+
+To run:
+```
+grunt serve
+```
+
+#### Contact
+ * [Twitter]
+
+Created with: [Dillinger]
+
+[StartBootstrap]:http://startbootstrap.com/
+[NodeJs]:http://nodejs.org/
+[GruntJs]:http://gruntjs.com/
+[Bower]:http://bower.io/
+
+[Twitter]:http://twitter.com/hugoarthur
+[Dillinger]:http://dillinger.io/
